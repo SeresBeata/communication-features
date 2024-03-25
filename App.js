b427a916941eaf1490f3e7daf52531cdd24b059d
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-03-25 11:09:18 +0100

    Update App.js to pick img from library
    Add functionality to "Pick an image from the library" btn by creating async function pickImage to grant permissions to media library. Import Image component. Use logical AND ( && ) operator to display the image as an Image element, if there is a picked image, otherwise display nothing. Use pickImage function reference in the onPress event listener of btn.

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
 import { useState } from 'react';
 
 //import ImagePicker to allow users to select a photo from their library or take a new photo.
@@ -8,10 +8,29 @@ const App = () => {
   //create state variable
   const [image, setImage] = useState(null);
 
+  //create async function to pick an image from the library
+  const pickImage = async () => {
+    let permissions = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (permissions?.granted) {
+      let result = await ImagePicker.launchImageLibraryAsync();
+
+      if (!result.canceled) setImage(result.assets[0]);
+      else setImage(null);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Button title="Pick an image from the library" />
+      <Button title="Pick an image from the library" onPress={pickImage} />
       <Button title="Take a photo" />
+      {/* Use logical AND ( && ) operator to display the image as an Image element, if there is a picked image, otherwise display nothing */}
+      {image && (
+        <Image
+          source={{ uri: image.uri }}
+          style={{ width: 200, height: 200, borderRadius: 5 }}
+        />
+      )}
     </View>
   );
 };
