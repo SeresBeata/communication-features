9d88a90f1609aafda9ddd81a19eb4df8430a4b00
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-03-25 10:27:05 +0100

    Refactor App.js
    Delete unnecessary parts. Use arrow function instead of the regular format to define App component.

--- App.js
+++ App.js
@@ -1,14 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App = () => {
+  return <View style={styles.container}>Hello World!</View>;
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -18,3 +12,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
