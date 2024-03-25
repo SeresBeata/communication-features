b6d5c2935206abac7ed5dc9eeb2574bc6f9a4692
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-03-25 15:23:39 +0100

    Update App.js for location data
    Import Location and MapView. Create state variable location to hold the location data. Create async function "getLocation" to get coordinates of the user’s location. Create "Get my location" btn. Use getLocation function reference in the onPress event listener of btn. Use logical AND ( && ) operator to display the user’s current location as a MapView element, if there is a location data, otherwise display nothing.

--- App.js
+++ App.js
@@ -7,9 +7,17 @@ import * as ImagePicker from 'expo-image-picker';
 //import MediaLibrary to save Photos captured using a device’s camera with the ImagePicker
 import * as MediaLibrary from 'expo-media-library';
 
+//import Location to retrieve the user’s location
+import * as Location from 'expo-location';
+
+//import MapView to display the location data in a map
+import MapView from 'react-native-maps';
+
 const App = () => {
   //create state variable
   const [image, setImage] = useState(null);
+  //define a state,that will hold the location data
+  const [location, setLocation] = useState(null);
 
   //create async function to pick an image from the library
   const pickImage = async () => {
@@ -42,15 +50,40 @@ const App = () => {
     }
   };
 
+  //create async function to get coordinates of the user’s location
+  const getLocation = async () => {
+    let permissions = await Location.requestForegroundPermissionsAsync();
+
+    if (permissions?.granted) {
+      const location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    } else {
+      Alert.alert("Permissions to read location aren't granted");
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Button title="Pick an image from the library" onPress={pickImage} />
       <Button title="Take a photo" onPress={takePhoto} />
+      <Button title="Get my location" onPress={getLocation} />
       {/* Use logical AND ( && ) operator to display the image as an Image element, if there is a picked image, otherwise display nothing */}
       {image && (
         <Image
           source={{ uri: image.uri }}
           style={{ width: 200, height: 200, borderRadius: 5 }}
+        />
+      )}
+      {/* Use logical AND ( && ) operator to display the user’s current location as a MapView element, if there is a location data, otherwise display nothing */}
+      {location && (
+        <MapView
+          style={{ width: 300, height: 200 }}
+          region={{
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
         />
       )}
     </View>
